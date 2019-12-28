82eac62ce68d672cd56c4bf540b7948ae7eecb33
WeCo <58941871+WeCo001@users.noreply.github.com>
2019-12-29 00:29:19 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_5
+{
+    class Program
+    {
+        class Program_1
+        {
+            struct Student
+            {
+                public int age;
+                public string name;
+            }
+            enum DayOfTime : long
+            {
+                Moring = 9,
+                Noon = 9,
+                Afernoon
+            }
+
+            static void Main(string[] args)
+            {
+                Student s1;
+                s1.age = 18;
+                Student[] ss = new Student[100];
+                DayOfTime d1;
+                d1 = DayOfTime.Noon;
+
+            }
+        }
+    }
+}
